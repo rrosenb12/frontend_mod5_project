3579feb137890cfb9808f5a6efdc5ff790e4a7c8
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-08-30 23:18:19 -0400

    added recursive func

--- src/Components/RedditCard.js
+++ src/Components/RedditCard.js
@@ -3,17 +3,19 @@ import React from 'react'
 export default class RedditCard extends React.Component{
 
     getUrl = (imgUrl) => {
-        let encoded = imgUrl.replace('amp;s', 's')
-        let doubleEncoded = encoded.replace('amp;', '')
-        let tripleEncoded = doubleEncoded.replace('amp;', '')
-        return tripleEncoded
+        if (imgUrl.includes('amp;')){
+            let encoded = imgUrl.replace('amp;', 's')
+            this.getUrl(encoded)
+        } else {
+            return imgUrl
+        }
     }
 
     render(){
     return(
         <div className="redditcard">
             <p className="reddittitle">{this.props.data.title}</p>
-                {this.props.data.preview ? <div className="redditimgcontainer"><img className="redditimg" src={this.getUrl(this.props.data.preview.images[0].source.url)} alt="" width="300" height="200"/></div>  : null}
+                {this.props.data.preview ? <div className="redditimgcontainer"><img className="redditimg" src={this.getUrl(this.props.data.preview.images[0].source.url.replace('amp;s', 's'))} alt="" width="300" height="200"/></div>  : null}
             
             <div className="reddithrefcontainer"><a href={`https://www.reddit.com${this.props.data.permalink}`} target="_blank" rel="noopener noreferrer" className="reddithref">{this.props.data.subreddit_name_prefixed}</a></div>
         </div>
