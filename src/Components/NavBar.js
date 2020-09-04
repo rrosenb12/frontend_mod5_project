f128577fd42b8b4f0a0b37a0b056fb8e22393802
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-09-03 20:45:08 -0400

    navbar now a function

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -7,43 +7,79 @@ import nooksilo from '../nooksilo.png'
 import isabellesilo from '../isabellesilo.png'
 import kksilo from '../kksilo.png'
 
-class NavBar extends React.Component{
+function NavBar(props) {
 
-    render(){
-        return(
-            <div className='navbar' onClick={this.props.unClickItem} >
-                <div className='buttons'>
-                    {this.props.currentUser === undefined || localStorage.length === 0 ? 
-                        <>
-                            <NavLink to='/login' className='navbarbutton' id="login">Login</NavLink> 
-                            <NavLink to='/signup'className='navbarbutton' id="signup">Signup</NavLink>
-                        </> 
-                    : 
-                        <>
-                            <button to='/logout' className='navbarbutton' id="logout" onClick={() => 
-                                    {   
-                                        this.props.logOutUser()
-                                    }
-                                }>Logout  
-                            </button>
-                            <NavLink to='/upload'className='navbarbutton' id="upload">Upload Image</NavLink>
-                            <NavLink to='/feed' className='navbarbutton' id='feed'>The Feed</NavLink>
-                        </>
-                    }
-                    <NavLink to='/search'className='navbarbutton' id="search">Search</NavLink>
-                    <NavLink to='/profile'className='navbarbutton' id="page">My Page</NavLink>
-                    <NavLink to='/gallery'className='navbarbutton' id='gallery'>Gallery</NavLink>
-                    <NavLink to='/'className='navbarbutton' id="home">Home</NavLink>
-                </div>
-                <div className='silos'>
-                    <img alt=" silo" className='silo' src={nooksilo} height="70"/>
-                    <img alt=" silo" className='silo' src={isabellesilo} height="70"/>
-                    <img alt=" silo" className='silo' src={kksilo} height="70"/>
-                </div>
+    return(
+        <div className='navbar' onClick={props.unClickItem} >
+            <div className='buttons'>
+                {props.currentUser === undefined || localStorage.length === 0 ? 
+                    <>
+                        <NavLink to='/login' className='navbarbutton' id="login">Login</NavLink> 
+                        <NavLink to='/signup'className='navbarbutton' id="signup">Signup</NavLink>
+                    </> 
+                : 
+                    <>
+                        <button to='/logout' className='navbarbutton' id="logout" onClick={() => 
+                                {   
+                                    props.logOutUser()
+                                }
+                            }>Logout  
+                        </button>
+                        <NavLink to='/upload'className='navbarbutton' id="upload">Upload Image</NavLink>
+                        <NavLink to='/feed' className='navbarbutton' id='feed'>The Feed</NavLink>
+                    </>
+                }
+                <NavLink to='/search'className='navbarbutton' id="search">Search</NavLink>
+                <NavLink to='/profile'className='navbarbutton' id="page">My Page</NavLink>
+                <NavLink to='/gallery'className='navbarbutton' id='gallery'>Gallery</NavLink>
+                <NavLink to='/'className='navbarbutton' id="home">Home</NavLink>
             </div>
-        )
-    }
+            <div className='silos'>
+                <img alt=" silo" className='silo' src={nooksilo} height="70"/>
+                <img alt=" silo" className='silo' src={isabellesilo} height="70"/>
+                <img alt=" silo" className='silo' src={kksilo} height="70"/>
+            </div>
+        </div>
+    )
 }
+
+// class NavBar extends React.Component{
+
+//     render(){
+//         return(
+//             <div className='navbar' onClick={this.props.unClickItem} >
+//                 <div className='buttons'>
+//                     {this.props.currentUser === undefined || localStorage.length === 0 ? 
+//                         <>
+//                             <NavLink to='/login' className='navbarbutton' id="login">Login</NavLink> 
+//                             <NavLink to='/signup'className='navbarbutton' id="signup">Signup</NavLink>
+//                         </> 
+//                     : 
+//                         <>
+//                             <button to='/logout' className='navbarbutton' id="logout" onClick={() => 
+//                                     {   
+//                                         this.props.logOutUser()
+//                                     }
+//                                 }>Logout  
+//                             </button>
+//                             <NavLink to='/upload'className='navbarbutton' id="upload">Upload Image</NavLink>
+//                             <NavLink to='/feed' className='navbarbutton' id='feed'>The Feed</NavLink>
+//                         </>
+//                     }
+//                     <NavLink to='/search'className='navbarbutton' id="search">Search</NavLink>
+//                     <NavLink to='/profile'className='navbarbutton' id="page">My Page</NavLink>
+//                     <NavLink to='/gallery'className='navbarbutton' id='gallery'>Gallery</NavLink>
+//                     <NavLink to='/'className='navbarbutton' id="home">Home</NavLink>
+//                 </div>
+//                 <div className='silos'>
+//                     <img alt=" silo" className='silo' src={nooksilo} height="70"/>
+//                     <img alt=" silo" className='silo' src={isabellesilo} height="70"/>
+//                     <img alt=" silo" className='silo' src={kksilo} height="70"/>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
 const mapStateToProps = state => {
     return {currentUser: state.currentUser.currentUser}
