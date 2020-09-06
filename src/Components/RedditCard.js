7e6fe5318d5c1c41e74525336866b0831d9bed47
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-09-06 12:01:34 -0400

    reddit logo added

--- src/Components/RedditCard.js
+++ src/Components/RedditCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import redditlogo from '../redditlogo.png'
 export default class RedditCard extends React.Component{
 
     truncateString = (string) => {
@@ -22,7 +23,7 @@ export default class RedditCard extends React.Component{
                 <div className='rtc'>
             <p className="reddittitle">{this.truncateString(this.props.data.title)}</p>
             </div>
-                {this.props.data.preview ? <div className="redditimgcontainer"><img className="redditimg" src={this.getUrl(this.props.data.preview.images[0].source.url.replace('amp;s', 's'))} alt=""/></div>  : null}
+                {this.props.data.preview ? <div className="redditimgcontainer"><img className="redditimg" src={this.getUrl(this.props.data.preview.images[0].source.url.replace('amp;s', 's'))} alt=""/></div>  : <div className="redditimgcontainer"><img className="redditimg" src={redditlogo} alt=""/></div>}
             
             <div className="reddithrefcontainer"><a href={`https://www.reddit.com${this.props.data.permalink}`} target="_blank" rel="noopener noreferrer" className="reddithref"> To the Post!</a></div>
             
