2f4e3e0879ceeadfc595a3f3436bdfbaeee8eb47
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-09-07 10:55:26 -0400

    setting author on gallery

--- src/Components/PhotoCard.js
+++ src/Components/PhotoCard.js
@@ -1,10 +1,25 @@
 import React from 'react'
 
+let users = []
+
+const fetchUsers = () => {
+    fetch('http://localhost:3000/users')
+    .then(response => response.json())
+    .then(usersArray => users = usersArray)
+}
+
+const setUser = photo => {
+    let author = users.find(user => user.id === photo.user_id)
+    return author.username
+}
+
 export default function PhotoCard(props){
     return(
         <div className="photocard">
-            <h1 className="photocardtitle"> {props.photo.description}</h1>
+            <h1 className="photocardtitle"> {props.photo.description}, by {setUser(props.photo)}</h1>
             <img className="photocardimg" alt="gallery" src={props.photo.image.url}/>
         </div>
     )
 }
+
+fetchUsers()
